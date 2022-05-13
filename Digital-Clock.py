77dd54131188e6b3966df71802dc67cc2defc274
Tevin Davis <tevindavis@icloud.com>
2022-05-12 20:49:50 -0400

    Added Stopwatch Components

--- Digital-Clock.py
+++ Digital-Clock.py
@@ -61,3 +61,49 @@ except:
           alarm_status_label.pack(anchor='s') 
 
           # Alarm Function #
+
+          def alarm(): #Takes the time from the datetime module and format it#
+                  main_time = datetime.datetime.now(). strftime("%H:%M %p")
+                  alarm_time = get_alarm_time_entry.get()
+                  alarm_time1, alarm_time2 = alarm_time.split('')
+                  alarm_hour, alarm_minutes = alarm_time1.split(' ')
+                  main_time1, main_time2 = main_time1.split(':')
+                   #This checks if the time eneterd is equal or not. If equal it will 'Beep' according to OS#
+                  if int(main_hour1) > 12 and int(main_hour1) < 24:
+                          main_hour = str(int(main_hour1) - 12)
+                  else:
+                          main_hour = main_hour1
+                  if int(alarm_hour) == int(main_hour) and int(alarm_minutes) == int(main_minutes) and main_time2 == alarm_time2:    
+                       for i in range(3):
+                               alarm_status_label.config(text = 'Time Is Up')
+                               if platform.system() == 'Windows':
+                                       winsound.beep(5000,1000)
+                                elif platform.system() == 'Darwin':
+                                        os.system('say Time Is Up')
+                                elif platfor.system() == 'Linux':
+                                        os.system('beep -f 5000')
+
+                        get_alarm_time_entry.config(state = 'enabled')
+                        set_alarm_button.cofig(state = 'enabled')
+                        get_alarm_time_entry.delete(0, END)
+                        alarm_status_label.cofig(text = '')
+
+                        else: 
+                                alarm_status_label.cofig(text ='Alarm Has Started')
+                                get_alarm_time_entry.config(state = 'disabled')
+                                set_alarm_button.config(state = 'disabled')
+                                alarm_status_label.after(1000, alarm) 
+
+
+                # Making Stopwatch Components #
+
+                stopwatch_label = label(stopwatch_tab, font = 'calibr 40 bold', text = 'Stopwatch')
+                stopwatch_label.pack(anchor = 'center')
+                stopwatch_start = Button (stopwatch_tab, text = 'Stop', state = 'disabled', command = lambda:stopwatch('start'))
+                stopwatch_start.pack(anchor = 'center')
+                stopwatch_stop = Button(stopwatch_tab, text = 'Stop', state = 'disabled', command=lambda:stopwatch('stop'))
+                stopwatch_stop.pack(anchor = 'center')
+                stopwatch_reset = Button(stopwatch_tab, text = 'Reset', state = 'disabled', command=lambda:stopwatch('reset'))
+                stopwatch_reset.pack(anchor = 'center')
+
+                 # Stopwatch Counter Function #                                        
