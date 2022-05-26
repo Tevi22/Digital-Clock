a3ee9b7e9d0b2b60b9d13602b79629a156237f9e
Tevin Davis <tevindavis@icloud.com>
2022-05-25 23:46:40 -0400

    Finished stopwatch function

--- Digital-Clock.py
+++ Digital-Clock.py
@@ -127,4 +127,29 @@ except:
 
                                         count()    
 
-                   # Adding stopwatch function #                     
+                   # Adding stopwatch function #
+
+                     def stopwatch(work):
+                        if work == 'start':
+                                global stopwatch_running
+                                stopwatch_running=True
+                                stopwatch_start.config(state='disable')
+                                stopwatch_stop.config(state='enabled')
+                                stopwatch_reset.config(state='enabled')
+                                stopwatch_counter(stopwatch_label)
+                          elif work == 'stop':
+                                  stopwatch_running=False
+                                  stopwatch_start.config(state='disabled')
+                                  stopwatch_stop.config(state='enabled')
+                                  stopwatch_reset.config(state='enabled')
+                          elif work = 'reset'
+                                global stopwatch_counter_num
+                                stopwatch_running=False
+                                stopwatch_counter_num=66600
+                                stopwatch_label.config(text='Stopwatch')
+                                stopwatch_start.config(state='enabled')
+                                stopwatch_stop.config(state='disabled')
+                                stopwatch_reset.config(state='disabled')
+
+                                #Making Timer#              
+
