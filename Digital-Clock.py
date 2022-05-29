28ab5ce0a730e26e0dca6d70eeebb8c013e9bc21
tevi22 <tevindavis@icloud.com>
2022-05-28 20:19:37 -0400

    fixed syntax and a fe bug

--- Digital-Clock.py
+++ Digital-Clock.py
@@ -1,3 +1,21 @@
+def execfile(file, glob=None, loc=None):
+    if glob is None:
+        import sys
+        glob = sys._getframe().f_back.f_globals
+    if loc is None:
+        loc = glob
+
+    # It seems that the best way is using tokenize.open(): http://code.activestate.com/lists/python-dev/131251/
+    import tokenize
+    stream = tokenize.open(file)  # @UndefinedVariable
+    try:
+        contents = stream.read()
+    finally:
+        stream.close()
+
+    #execute the script (note: it's important to compile first to have the filename set in debug mode)
+    exec(compile(contents+"\n", file, 'exec'), glob, loc)
+
 from tkinter import *  # importing modules#
 from tkinter.ttk import *
 import datetime
@@ -5,7 +23,7 @@ import platform
 try:
         import winsound #windows
         type='window'
-except:
+finally:
         import os #other
         type='other'
 
@@ -24,45 +42,43 @@ timer_running = False
 
 def clock():
         date_time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S/%p")
-        date,time1 = date_time.split(
+        date,time1 = date_time.split()
         time2, time3 = time.split('/')
         hour, minutes, seconds = time2.split(':')
-        if int(hour) > and init(hour) < 24:
+        if int(hour) > 11 and init(hour) < 24:
                 time = str(int(hours) - 12) + ':' + minutes + ':' + seconds + ' ' + time3
         else:
                    time = time2 + ' ' + time3
-                time_label.config(text=time)
-                date_label.config(text=date)
-                time_label.after(1000, clock)        
-                )
+                   time_label.config(text=time)
+                   date_label.config(text=date)
+                   time_label.after(1000, clock)
            # Alarm Function #
-def alarm(): 
-#Takes the time from the datetime module and format it#
+def alarm(): #Takes the time from the datetime module and format it#
           main_time = datetime.datetime.now(). strftime("%H:%M %p")
           alarm_time = get_alarm_time_entry.get()
           alarm_time1, alarm_time2 = alarm_time.split('')
           alarm_hour, alarm_minutes = alarm_time1.split(':')
           main_time1, main_time2 = main_time1.split('')
           main_hour1, main_minutes = main_time1.splitt(':')
-             #This checks if the time eneterd is equal or not. If equal it will 'Beep' according to OS#
-                  if main_time2 == 'PM':
-                          main_hour = str(int(main_hour1) - 12)
-                  else:
+          #This checks if the time eneterd is equal or not. If equal it will 'Beep' according to OS#
+if main_time2 == 'PM':
+        main_hour = str(int(main_hour1) - 12)
+else:
                           main_hour = main_hour1
-                  if int(alarm_hour) == int(main_hour) and int(alarm_minutes) == int(main_minutes) and main_time2 == alarm_time2:    
-                       for i in range(3):
+if int(alarm_hour) == int(main_hour) and int(alarm_minutes) == int(main_minutes) and main_time2 == alarm_time2:
+                for i in range(3):
                                alarm_status_label.config(text = 'Time Is Up')
                                if platform.system() == 'Windows':
                                        winsound.beep(5000,1000)
-                                elif platform.system() == 'Darwin':
+                               elif platform.system() == 'Darwin':
                                         os.system('say Time Is Up')
-                                elif platfor.system() == 'Linux':
+                               elif platfor.system() == 'Linux':
                                         os.system('beep -f 5000')
-                        get_alarm_time_entry.config(state = 'enabled')
-                        set_alarm_button.cofig(state = 'enabled')
-                        get_alarm_time_entry.delete(0, END)
-                        alarm_status_label.cofig(text = '')
-                 else: 
+                get_alarm_time_entry.config(state = 'enabled')
+                set_alarm_button.cofig(state = 'enabled')
+                get_alarm_time_entry.delete(0, END)
+                alarm_status_label.cofig(text = '')
+else:
                                 alarm_status_label.cofig(text ='Alarm Has Started')
                                 get_alarm_time_entry.config(state = 'disabled')
                                 set_alarm_button.config(state = 'disabled')
@@ -72,16 +88,16 @@ def stopwatch_counter(label):
         def count():
                 if stopwatch_running:
                         global stopwatch_counter_num
-                        if stopwatch_counter_num=66600:
+                        if stopwatch_counter_num(66600):
                                 display = "Starting..."
-                         else:
+                        else:
                                  tt= datetime.datetime.fromtimestamp(stopwatch_counter_num)
                                  string= tt.strftime("%H:%M:%S")
                                  display=string
                                  label.cofig(text=display)
-                                label.after(1000, count)
-                                stopwatch_counter_num += 1   
-                           count()
+                                 label.after(1000, count)
+                                 stopwatch_counter_num += 1
+                                 count()
  # Adding stopwatch function #
 def stopwatch(work):
                 if work == 'start':
@@ -89,14 +105,14 @@ def stopwatch(work):
                         stopwatch_running=True
                         stopwatch_start.config(state='disable')
                         stopwatch_stop.config(state='enabled')
-                       stopwatch_reset.config(state='enabled')
-                       stopwatch_counter(stopwatch_label)
+                        stopwatch_reset.config(state='enabled')
+                        stopwatch_counter(stopwatch_label)
                 elif work == 'stop':
                                   stopwatch_running=False
                                   stopwatch_start.config(state='disabled')
                                   stopwatch_stop.config(state='enabled')
                                   stopwatch_reset.config(state='enabled')
-                elif work = 'reset'
+                elif work =='reset':
                                 global stopwatch_counter_num
                                 stopwatch_running=False
                                 stopwatch_counter_num=66600
@@ -106,51 +122,51 @@ def stopwatch(work):
                                 stopwatch_reset.config(state='disabled')                                                 
                 # Creating timer_counter#
 def timer_counter(label):
-        def count();
+        def count():
           global timer_running
           if timer_running:
                   global timer_counter_num
-                  if timer_counter_num==66600
+                  if timer_counter_num(66600):
                    for i in range(3):
                            display="Time is up"
                            if platform.system() == 'Window':
                                    winsound.Beep(5000,1000)
-                           elif platform.system( == 'Darwin'):
+                           elif platform.system() == 'Darwin':
                                    os.system('say Time is Up')
                            elif platformm.system() == 'Linux':
                                    os.system('beep -f 5000')
-                        timer_running = False
-                        timer('reset')
-                     else:
+                                   timer_running = False
+                                   timer('reset')
+                           else:
                              tt = datetime.datetime.fromtimestamp(timer_counter_num)
                              string = tt.strftime("%H:%M:%S")
                              display = string
                              timer_counter_num -= 1
-                        label.config(text=display)
-                        label.after(1000, count)
+                             label.config(text=display)
+                             label.after(1000, count)
         count()                                                                   
 #The following code will be called by the timer buttons#
 def timer(work):
         if work == 'start':
                         global timer_running, timer_counter_num
                         timer_running=True
-                                        if timer_counter_num == 66600:
+                        if timer_counter_num(66600):
                                                 timer_time_str = timer_get_entry.get()
                                                 hours,minutes,seconds=timer_time_str.split(':')
                                                 minutes = int(minutes) + (int(hours) * 60)
                                                 seconds = int(seconds) + (minutes * 60)
                                                 timer_counter_num= timer_counter_num + seconds
-                                            timer_counter(timer_label)
-                                            timer_start.config(state='disabled')
-                                            timer_stop.config(state='enabled')
-                                            timer_reset.config(state='enabled')
-                                            timer_get_entry.delete(0,END)
-                                        elif work == 'stop':
-                                             timer_runniing=False
+                                                timer_counter(timer_label)
+                                                timer_start.config(state='disabled')
+                                                timer_stop.config(state='enabled')
+                                                timer_reset.config(state='enabled')
+                                                timer_get_entry.delete(0,END)
+                        elif work == 'stop':
+                                             timer_running=False
                                              timer_start.config(state='enabled')
                                              timer_stop.config(state='disabled')
                                              timer_reset.config(state='enabled')
-                                        elif work == 'reset':
+                        elif work == 'reset':
                                                 timer_runniing=False
                                                 timer_counter_num=66600
                                                 timer_start.config(state='enabled')
@@ -194,11 +210,11 @@ stopwatch_stop.pack(anchor = 'center')
 stopwatch_reset = Button(stopwatch_tab, text = 'Reset', state = 'disabled', command=lambda:stopwatch('reset'))
 stopwatch_reset.pack(anchor = 'center')
  #Making Timer button#  
- timer_get_entry = Entry(timer_tab, font = 'calibriri 15 bold')
+timer_get_entry = Entry(timer_tab, font = 'calibriri 15 bold')
 timer_get_entry.pack(anchor='center')
-timer_instructions_label = Lable(timer_tab, font = 'calibri 10 bold', text = 'Enter Timer Time. Eg -> 01:30:30, 01 -> Hour, 30  -> Minutes, 30 -> Seconds')
+timer_instructions_label = Label(timer_tab, font = 'calibri 10 bold', text = 'Enter Timer Time. Eg -> 01:30:30, 01 -> Hour, 30  -> Minutes, 30 -> Seconds')
 timer_instructions_label.pack(anchor='s')
-time_label = Label(timer_tab, font='calibri 40 bold', text='Timer')
+timer_label = Label(timer_tab, font='calibri 40 bold', text='Timer')
 timer_label.pack(anchor='center')
 timer_start = Button(timer_tab, text ='Start', command= lambda:timer('start'))
 timer_start.pack(anchor='center')
